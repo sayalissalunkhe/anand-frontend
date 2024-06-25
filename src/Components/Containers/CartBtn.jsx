3062ae4449fb41aedecf72fb32ce4e5696787c69
Sayali Rane <sayalisalunkhe2496@gmail.com>
2024-06-25 18:56:29 +0530

    Updated navigate to login if add to cart

--- src/Components/Containers/CartBtn.jsx
+++ src/Components/Containers/CartBtn.jsx
@@ -1,56 +1,70 @@
 import axios from 'axios';
-import { toast } from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
 import { CheckCartBucket } from '../../Helpers';
 import { addToCart, removeFromCart, setTestCartList } from '../../Redux/Actions/TestAction';
 import { API_URL } from '../../Redux/Constant/ApiRoute';
 
 export default function CartBtn(props) {
-    const dispatch  = useDispatch();
-    const user      = localStorage.getItem('user')
+    const dispatch = useDispatch();
+    const user = localStorage.getItem('user');
 
     const addToCartItem = () => {
-        if(user !== null ) {
-            axios.post(API_URL.ADD_TO_CART,{
-                user_id : JSON.parse(user).id,
-                test_id : props.testData.id,
-                test_type : props.testData.IsPackage == 'No' ? 'TEST' : 'PACKAGE',
+        if (user !== null) {
+            axios.post(API_URL.ADD_TO_CART, {
+                user_id: JSON.parse(user).id,
+                test_id: props.testData.id,
+                test_type: props.testData.IsPackage === 'No' ? 'TEST' : 'PACKAGE',
             }).then((response) => {
-                console.log(response.data)
-            }) 
-        }  
-        dispatch(addToCart(props.testData));
-        dispatch(
-            setTestCartList(
-                JSON.parse(localStorage.getItem("CartTestList"))
-            )
-        );
+                console.log(response.data);
+                // Update local storage and state after adding to cart
+                const updatedCartList = JSON.parse(localStorage.getItem("CartTestList")) || [];
+                updatedCartList.push(props.testData);
+                localStorage.setItem("CartTestList", JSON.stringify(updatedCartList));
+                dispatch(setTestCartList(updatedCartList));
+                dispatch(addToCart(props.testData));
+            }).catch(error => {
+                console.error("Error adding to cart:", error);
+            });
+        } else {
+            // Redirect to login page or show login prompt
+            window.location.href = "/login"; // Redirect to login page
+        }
     }
 
     const removeToCart = () => {
-        if(user !== null ) {
-            axios.post(API_URL.REMOVE_TO_CART,{
-                user_id : JSON.parse(user).id,
-                test_id : props.testData.id,
-                test_type : props.testData.IsPackage == 'No' ? 'TEST' : 'PACKAGE',
+        if (user !== null) {
+            axios.post(API_URL.REMOVE_TO_CART, {
+                user_id: JSON.parse(user).id,
+                test_id: props.testData.id,
+                test_type: props.testData.IsPackage === 'No' ? 'TEST' : 'PACKAGE',
             }).then((response) => {
-                console.log(response.data)
-            })
+                console.log(response.data);
+                // Update local storage and state after removing from cart
+                const updatedCartList = JSON.parse(localStorage.getItem("CartTestList")) || [];
+                const updatedCartListFiltered = updatedCartList.filter(item => item.id !== props.testData.id);
+                localStorage.setItem("CartTestList", JSON.stringify(updatedCartListFiltered));
+                dispatch(setTestCartList(updatedCartListFiltered));
+                dispatch(removeFromCart(props.testData));
+            }).catch(error => {
+                console.error("Error removing from cart:", error);
+            });
+        } else {
+            // Show an error message if the user is not logged in
+            window.location.href = "/login"; // Redirect to login page
         }
-        dispatch(removeFromCart(props.testData));
-        dispatch(setTestCartList(JSON.parse(localStorage.getItem("CartTestList"))));
     }
-    if(CheckCartBucket(props.testData.TestId))  {
+
+    if (CheckCartBucket(props.testData.TestId)) {
         return (
-            <button  className="cart-btn remove-btn" onClick={() => removeToCart()}>
+            <button className="cart-btn remove-btn" onClick={() => removeToCart()}>
                 <small><i className="fa fa-times mr-2"></i>Remove</small>
             </button>
-        )
-    } else  {
+        );
+    } else {
         return (
             <button className="cart-btn text-white choose-your-package" onClick={() => addToCartItem()} >
                 <i className="fa fa-cart-plus mr-1"></i>ADD
             </button>
-        )
+        );
     }
 }
--- src/Components/Includes/CartCount.jsx
+++ src/Components/Includes/CartCount.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import cart from './../../assets/images/cart.png';
+import { Link, useNavigate } from 'react-router-dom';
+import cart from '../../assets/images/cart.png'; // Make sure to adjust the path to your cart image
+import { toast } from 'react-hot-toast';
 
 export default function CartCount() {
-    const cartItemCount  = useSelector((state) => state.TestCartList.cartTest);
+    const cartItemCount = useSelector((state) => state.TestCartList.cartTest);
+    const navigate = useNavigate();
+
+    const handleCartClick = (event) => {
+        const user = localStorage.getItem('user');
+        if (!user) {
+            event.preventDefault(); // Prevent the default link behavior
+            toast.error("Please log in to view your cart.");
+            navigate('/login'); // Redirect to the login page
+        }
+    };
 
     return (
-        <Link to='/my-cart' className='position-relative'>
-            <img src={cart} alt="" className="img-fluid"/>
-                {
-                    cartItemCount !== undefined 
-                    ? 
-                        
-                        cartItemCount.length !== 0 ?
-                            <b className="badge badge-dark cart-count">{cartItemCount.length}</b>
-                        : null
-                    : 
-                        localStorage.getItem('CartTestList') !== null 
+        <Link to='/my-cart' className='position-relative' onClick={handleCartClick}>
+            <img src={cart} alt="" className="img-fluid" />
+            {
+                cartItemCount !== undefined
                     ?
-                        JSON.parse(localStorage.getItem('CartTestList')).length !== 0 ? 
-                        <b className="badge badge-dark cart-count">{JSON.parse(localStorage.getItem('CartTestList')).length}</b>
+                    cartItemCount.length !== 0 ?
+                        <b className="badge badge-dark cart-count">{cartItemCount.length}</b>
                         : null
-                    :   null
-                }
+                    :
+                    localStorage.getItem('CartTestList') !== null
+                        ?
+                        JSON.parse(localStorage.getItem('CartTestList')).length !== 0 ?
+                            <b className="badge badge-dark cart-count">{JSON.parse(localStorage.getItem('CartTestList')).length}</b>
+                            : null
+                        : null
+            }
             <span>Cart</span>
         </Link>
-    )
+    );
 }
